04c33b72b67c04392a8062c52fd6f4b5c2f2ca44
Claude Falguiere <cfalguiere@gmail.com>
2022-10-27 16:50:25 +0000

    added noxfile

---
+++ noxfile.py
@@ -0,0 +1,16 @@
+import nox
+
+
+nox.options.reuse_existing_virtualenvs = True
+
+@nox.session
+def init(session):
+    session.install("-r", "venv-config/requirements.txt")
+    session.run("python", "-m", "ipykernel", "install", "--user", "--name=init", "--display-name", "Python 3.9 (iris)")
+
+
+@nox.session
+def lint(session):
+    session.install("-r", "venv-config/requirements.txt")
+    session.install("flake8")
+    session.run("flake8", "noxfile.py")
